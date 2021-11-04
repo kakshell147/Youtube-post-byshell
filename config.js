94ade054423831498cb109ed1da905e598f4e2b1
kakshell147 <93724110+kakshell147@users.noreply.github.com>
2021-11-04 08:38:42 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "",
+    channel: "833853700803264555",
+    messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
+    channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
+    watchInterval: 30000
+}
